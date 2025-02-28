457b6ed5a519c2dc93d188fd20bcc8cf17b3f53d
OrukCagatay <cagatayoruk@posta.mu.edu.tr>
2025-02-28 12:07:45 +0300

    week 2

---
+++ src/Dt.java
@@ -0,0 +1,17 @@
+public class Dt {
+
+    public static void main(String[] args) {
+
+        int grade = 1;
+        double grade_2 = 1.5;
+        boolean is_ok = true;
+        char first = 'A';
+
+        System.out.println(grade);
+        System.out.println(grade_2);
+        System.out.println(is_ok);
+        System.out.println(first);
+
+
+    }
+}
